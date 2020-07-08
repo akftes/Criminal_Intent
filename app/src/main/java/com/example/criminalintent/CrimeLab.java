a040a193357b68da7d752faaa8415aa539244016
akftes <akftes@gmail.com>
2020-07-08 20:44:19 +0430

    add mCrimes List

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -2,9 +2,11 @@ package com.example.criminalintent;
 
 import android.content.Context;
 
+import java.util.List;
+
 public class CrimeLab {
     private  static  CrimeLab  sCrimeLab;
-
+    git private List<Crime> mCrimes;
     public static CrimeLab get(Context context){
         if(sCrimeLab==null){
             sCrimeLab = new CrimeLab(context);
